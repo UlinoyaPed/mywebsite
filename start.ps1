602f4516587804874e69cd6c277730e0e5705a68
UlinoyaPed <l15010104769@outlook.com>
2023-02-03 17:16:16 +0800

    test\(script\)\:

--- start.ps1
+++ start.ps1
@@ -1,6 +1,6 @@
 ﻿function main {
     # 定义功能
-    $funcs = "Add_a_new_mdFile", "Test_local", "Build_and_deploy", "Push_to_git_and_deploy", "Git_submodule_upgrade"
+    $funcs = "Add_a_new_mdFile", "Test_local", "Build", "Push_to_git_Auto", "Push_to_git_Manual", "Git_submodule_upgrade"
 
     for ($x = 0; $x -lt $funcs.Length; $x = $x + 1) {
         Write-Output "$($x) $($funcs[$x])"
@@ -9,7 +9,7 @@
     # 执行代号对应的功能
     Invoke-Expression $($funcs[$inpt])
     Set-Location $PSScriptRoot
-    
+
     Write-Output "Finish!"
 }
 
@@ -23,12 +23,42 @@ function Add_a_new_mdFile {
 function Test_local {
     hugo serve --disableFastRender -D
 }
-function Build_and_deploy {
-    netlify deploy --build --prod
+function Build {
+    Remove-Item .\public -Recurse
+    hugo --gc --minify -e production
 }
-function Push_to_git_and_deploy {
+function Push_to_git_Auto {
     git add .
     git commit -m "网站更新 From 自动脚本"
+    git push
+}
+function Push_to_git_Manual {
+    git add .
+
+    # 定义commit type
+    $headertype = "feat", "fix", "docs", "style", "refactor", "test", "chore"
+    $headertype_cn = "新功能/新文章", "修改Bug/修改文章", "添加文档", "优化格式", "重写代码/重写文章", "测试功能", "修改构建过程/添加辅助工具"
+    # 循环输出
+    for ($x = 0; $x -lt $headertype_cn.Length; $x = $x + 1) {
+        Write-Output "$($x) $($headertype_cn[$x])"
+    }
+    $inpttype = Read-Host "选择提交类别（必须）"
+    $CommitType = $($headertype[$inpttype])
+
+    $headerscope = "posts", "script", "css", "theme", "picture", "config"
+    $headerscope_cn = "文章", "脚本", "CSS", "主题", "图片", "配置"
+    # 循环输出
+    for ($x = 0; $x -lt $headerscope_cn.Length; $x = $x + 1) {
+        Write-Output "$($x) $($headerscope_cn[$x])"
+    }
+    $inptscope = Read-Host "选择提交影响范围（必须）"
+    $CommitScope = $($headerscope[$inptscope])
+
+    $CommitSubject = Read-Host "输入提交信息（非必须）"
+
+    $CommitMessage = "$($CommitType)\($($CommitScope)\)\: $($CommitSubject)"
+
+    git commit -m "$($CommitMessage)"
     git push
 }
 function Git_submodule_upgrade {
